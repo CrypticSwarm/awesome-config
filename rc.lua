01b11035f6f3ef82f70454129f4a875c4dbca2ad
CrypticSwarm <crypticswarm+git@gmail.com>
2017-07-01 17:37:16 -0500

    add local to another varaiable

--- rc.lua
+++ rc.lua
@@ -278,7 +278,7 @@ for i = 1, keynumber do
     )
 end
 
-clientbuttons = awful.util.table.join(
+local clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
     awful.button({ modkey }, 3, awful.mouse.client.resize))
