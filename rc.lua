17006469a10c2e56d4b0a2b76e0ff65fe61b2ebd
CrypticSwarm <crypticswarm+git@gmail.com>
2017-07-01 16:56:25 -0500

    removed unused window rules

--- rc.lua
+++ rc.lua
@@ -289,19 +289,12 @@ root.keys(globalkeys)
 
 -- {{{ Rules
 awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
+    { rule = {},
       properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true, tag = tags[1][7] } },
+                     buttons = clientbuttons } }
 }
 -- }}}
 
